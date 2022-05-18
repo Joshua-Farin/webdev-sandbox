99e4047b27626dbe49a46727fbfb85c1a78429d7
Joshua Farin <joshua.k.farin@gmail.com>
2022-05-18 02:33:33 -1000

    Test images

--- components/Projects.js
+++ components/Projects.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
+import profilePic from '../public/1.png'
+
 
 function Projects() {
   return (
     <div>
       
       {/* Title, 4 project cards */}
+      <title>Projects</title>
+
+      <div className='flex'>
+        <div>
+          <Image src={profilePic} width={300} height={200}/>
+          <p>Spotify Clone</p>
+        </div>
+        <div>
+          <Image src={profilePic} width={300} height={200}/>
+          <p>Hulu Clone</p>
+        </div>
+        <div>
+          <Image src={profilePic} width={300} height={200}/>
+          <p>Google Clone</p>
+        </div>
+        <div>
+          <Image src={profilePic} width={300} height={200}/>
+          <p>Medium Clone</p>
+        </div>
+      </div>
 
     </div>
   )
